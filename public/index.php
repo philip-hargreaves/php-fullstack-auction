a4567858883636441a9c965c70f930c93fd28c17
Philip Hargreaves <philiphargreaves@live.co.uk>
2025-11-03 15:45:35 +0000

    Add basic routing logic to entry point

--- public/index.php
+++ public/index.php
@@ -1,2 +1,19 @@
 <?php
-echo "If you can see this, docker is working correctly :)";
+require "../Core/utilities.php";
+
+
+// Extract clean path from URI
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+
+$routes = [
+    "/" => "../controllers/index.php",
+];
+
+// Load and execute the corresponding controller if route exits
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+    echo "404 Page Not Found";
+    die();
+}
