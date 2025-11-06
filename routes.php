9a6cedbef1f70e98c11f263154c692d25c84cf70
Philip Hargreaves <philiphargreaves@live.co.uk>
2025-11-06 05:39:25 +0000

    Tidy up routes.php

--- routes.php
+++ routes.php
@@ -4,10 +4,10 @@
 $router->get('/', '../controllers/index.php');
 
 // Create Auction Page (GET displays the form, POST submits the data)
-$router->get('/create-auction', '../controllers/create-auction.php'); // Display Form
-$router->post('/create-auction', '../controllers/create-auction-result.php'); // Process Form Submission
+$router->get('/create-auction', '../controllers/create-auction.php');
+$router->post('/create-auction', '../controllers/create-auction-result.php');
 
-// Other GET Pages
+// GET Pages
 $router->get('/listing', '../controllers/listing.php');
 $router->get('/my-listings', '../controllers/my-listings.php');
 $router->get('/register', '../controllers/register.php');
