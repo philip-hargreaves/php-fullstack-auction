e8b7324f4bd5e2bb09485571cef23b3d079cb406
Philip Hargreaves <philiphargreaves@live.co.uk>
2025-11-06 05:07:26 +0000

    Implement Router class with GET/POST methods

---
+++ Core/Router.php
@@ -0,0 +1,38 @@
+<?php
+
+class Router {
+    protected $routes = [
+        'GET' => [],
+        'POST' => [],
+    ];
+
+    public function get($uri, $controller)
+    {
+        // eg ['GET']['/listing'] = 'controller/listing.php'
+        $this->routes['GET'][$uri] = $controller;
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->routes['POST'][$uri] = $controller;
+    }
+
+    // Delete, Put, Patch to be added when needed
+
+    public function route($uri, $method)
+    {
+        if ($this->routes[$method][$uri] ?? false) {
+            return require $this->routes[$method][$uri];
+            }
+        $this->abort();
+    }
+
+    protected function abort($code = 404)
+    {
+        http_response_code($code);
+        dd("{$code} Page Not Found");
+    }
+}
+
+
+
