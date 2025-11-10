d9bef7acc582e30dfed0cf8db03b5a3365c41264
Sabrina9178 <ssss9178@gmail.com>
2025-11-10 04:42:02 +0000

    feat: finish basic auction.php

--- controllers/auction.php
+++ controllers/auction.php
@@ -7,54 +7,57 @@ require_once base_path('app/repositories/AuctionRepository.php');
 
 $auction_id = $_GET['auction_id'];
 
-// Use auction_id to make a query to the database
+// Dependency Injection
 $db = new Database();
-$userRepo = new UserRepository();
+$roleRepo = new RoleRepository($db);
+$userRepo = new UserRepository($db, $roleRepo);
 $itemRepo = new ItemRepository($db, $userRepo);
 $auctionRepo = new AuctionRepository($db, $itemRepo);
 
+// Use auction_id to make a query to the database
 $auction = $auctionRepo->getAuctionByAuctionId($auction_id);
 $item = $auction->getItem();
+$bids = []; // Get all bids from BidRepo
 
 // Variables required in the view
 $title = $item->getItemName();
-$sellerID = $item->getSellerID();
+$sellerName = $item->getSeller()->getUsername();
 $description = $item->getItemDescription();
-$current_price = 30.50;
-$num_bids = 1;
-$start_time = $auction->getStartDateTime();
-$end_time = $auction->getEndDateTime();
-$starting_price = $auction->getStartingPrice();
-$reserve_price = $auction->getReservePrice();
-$auction_status = $auction->getAuctionStatus();
-//dummy
+$currentBids = [];
+$currentPrice = 0;
+$startTime = $auction->getStartDateTime();
+$endTime = $auction->getEndDateTime();
+$startingPrice = $auction->getStartingPrice();
+$reservePrice = $auction->getReservePrice();
+$auctionStatus = $auction->getAuctionStatus();
 
-// TODO: Note: Auctions that have ended may pull a different set of data,
-//       like whether the auction ended in a sale or was cancelled due
-//       to lack of high-enough bids. Or maybe not.
-
-// Calculate time to auction end:
-$now = new DateTime();
-
-if ($auction_status == 'finished') {
-    $item_status = $item->getItemStatus();
-    if ($item_status == 'available'){
-
-    } else {
-
+// Display different data for different $auctionStatus and $itemStatus
+if ($auctionStatus == 'active') {
+    $now = new DateTime();
+    $time_remaining = $now->diff($endTime);
+} else if ($auctionStatus == 'finished') {
+    $itemStatus = $item->getItemStatus();
+    if ($itemStatus == 'available') {
+        $auctionResult = 'Unsold';
+    } else if ($itemStatus == 'sold') {
+        $auctionResult = 'Sold';
+    } else if ($itemStatus == 'deleted') {
+        // Jump to 404 not found
     }
-} else if ($auction_status == 'reserved') {
-
 }
 
-// TODO: If the user has a session, use it to make a query to the database
-//       to determine if the user is already watching this item.
-//       For now, this is hardcoded.
-$has_session = true;
-$watching = false;
+// Session Status
+if (session_status() === PHP_SESSION_ACTIVE) {
+    // Define actions restricted users
+    $has_session = true;
+    $user = $userRepo->getUserByUserId($_SESSION['user_id']);
+
+    //$isWatched = WatchlistRepository->getIsWatchedByUserIdAndAuctionId($_SESSION['user_id'], $auction->getAuctionId());
+    $isWatched = false;
+} else {
+    $has_session = false;
+}
 
 
-// 4. At the very end, load the "View"
-// This "passes" all the single-item variables ($item_id, $title,
-// $description, etc.) into the 'views/auction.view.php' file.
+
 require base_path('views/auction.view.php');
