286b6d6b465ba67d908c18ed8211f2828710bbf5
Sabrina9178 <ssss9178@gmail.com>
2025-11-10 04:01:40 +0000

    feat: add getUserByUserId() in UserRepository

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -38,4 +38,32 @@ class UserRepository {
 
         return $object;
     }
+
+    // Should extract common parts of getUserByEmail and getUserByUserId
+    public function getUserByUserId(int $userId) : ?User {
+        // Query to get the User (without roles)
+        // Select only from the users table.
+        $queryRow = "SELECT id, username, email, password, is_active FROM users WHERE id = :userId";
+        $row = $this->db->query($queryRow, ['userId' => $userId])->fetch();
+
+        // Check if user exists
+        if (empty($row)) return null;
+
+        // Create the User object using constructor
+        $object = new User(
+            (int)$row['id'],
+            $row['username'],
+            $row['email'],
+            $row['password'],
+            (bool)$row['is_active']
+        );
+
+        // Ask the RoleRepository to get the Role objects by userID
+        $roles = $this->roleRepo->getRolesByUserId($object->getUserID());
+
+        // Set the array of real Role objects on the user
+        $object->setRoles($roles);
+
+        return $object;
+    }
 }
