918f27c70660d92b1489be90991d38838c04e780
Sabrina9178 <ssss9178@gmail.com>
2025-11-14 01:24:24 +0000

    refactor: add DIContainer.php, bootstrap.php, and clean up all the original DI logic

---
+++ infrastructure/DIContainer.php
@@ -0,0 +1,19 @@
+<?php
+namespace infrastructure;
+class DependencyContainer {
+    private static array $registry = [];
+
+    public static function bind(string $key, $value) : void
+    {
+        static::$registry[$key] = $value;
+    }
+
+    public static function get(string $key)
+    {
+        if (!array_key_exists($key, static::$registry)) {
+//            throw new Exception("No service is bound with the key: {$key}");
+        }
+
+        return static::$registry[$key];
+    }
+}
