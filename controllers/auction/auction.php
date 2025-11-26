786f113c5edf8309c75b42ec4798cbb77a3ebcfd
Sabrina9178 <ssss9178@gmail.com>
2025-11-26 16:43:12 +0000

    Add extra feature's tables to db schema (#37)
    * add extra feature tables to db schema
    
    # Conflicts:
    #	db/schema.sql
    
    * add ON DELETE and add auction_id FK to conversations table
    
    * fix schema issue: confilcts between ON DELETE SET NULL and NOT NOULL
    
    * Deleted item_status
    
    # Conflicts:
    #	db/schema.sql
    
    * Replaced item_status with auction_status
    
    * A slight change in schema, forgot to reset auction_service
    
    * change is_main_image to is_main
    
    ---------
    
    Co-authored-by: Martin <martinchaouk@gmail.com>

--- controllers/auction/auction.php
+++ controllers/auction/auction.php
@@ -49,7 +49,6 @@ $imageUrls = [
 $currencyText = '£';
 
 // Variables changes with $auctionStatus
-$itemStatus = $item->getItemStatus();
 $auctionStatus = $auction->getAuctionStatus();
 $isAuctionActive = $auctionStatus == 'Active';
 $bidText = "";
@@ -63,19 +62,20 @@ if ($auctionStatus == 'Active') {
     $bidText = "Current Bid";
     $statusText = "Reserve price not yet met.";
     $timeRemaining = $now->diff($endTime);
-} else if ($auctionStatus == 'Finished') {
+} else if ($auctionStatus == 'Sold' || $auctionStatus == 'Unsold' || $auctionStatus == 'Deleted') {
+
     $bidText = "Final Bid";
     $timeRemaining = $now->diff($now);
     // Display auction result
-    if ($itemStatus == 'Available') {
+    if ($auctionStatus == 'Sold') {
         $statusText = "This auction ends. The item is sold";
-    } else if ($itemStatus == 'Sold') {
+    } else if ($auctionStatus == 'Unsold') {
         $statusText = "This auction ends. The item is not sold.";
         $winningBid = $bidServ->getWinningBidForAuction($auctionId);
-    } else { //$itemStatus == 'deleted'
+    } else { //$auctionStatus == 'Deleted'
         // Jump to 404 not found
     }
-} else { //$auctionStatus == 'Pending'
+} else { //$auctionStatus == 'Scheduled'
     $bidText = "Current Bid";
     $statusText = "This auction hasn't start yet.";
     $timeRemaining = $now->diff($endTime);
