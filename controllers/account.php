fd40a8b43f59aef5a69ab33855c01910debb3435
Martin <martinchaouk@gmail.com>
2025-11-26 16:35:33 +0000

    Clicking on your own profile and other people's profiles will display different information

--- controllers/account.php
+++ controllers/account.php
@@ -4,29 +4,35 @@ use app\services\AuctionService;
 use app\services\AuthService;
 use infrastructure\DIContainer;
 use infrastructure\Utilities;
+use infrastructure\Request;
 
-$userId = AuthService::getUserId();
-if ($userId === null) {
+$currentUserId = AuthService::getUserId();
+$targetUserId = Request::get('user_id');
+
+if ($targetUserId) {
+    $targetUserId = (int)$targetUserId;
+} elseif ($currentUserId) {
+    $targetUserId = $currentUserId;
+} else {
     header('Location: /');
     exit;
 }
 
 $userService = DIContainer::get('userService');
 
-$user = $userService->getUserAccount($userId);
+$user = $userService->getUserAccount($targetUserId);
 
 if ($user === null) {
     http_response_code(404);
-    Utilities::dd("404 User Not Found");
+    Utilities::dd("User Not Found");
 }
+
+$isOwnProfile = ($currentUserId === $targetUserId);
+$targetUserRoles = $user->getRoleNames();
+$isTargetUserSeller = in_array('seller', $targetUserRoles);
 
 $auctionServ = DIContainer::get('auctionServ');
-
-$isSeller = AuthService::hasRole('seller');
-$activeAuctions = [];
-
-if ($isSeller) {
-    $activeAuctions = $auctionServ->getAuctionsForSeller($userId);
-}
+$activeAuctions = $auctionServ->getAuctionsForSeller($targetUserId);
+$showSellerSection = $isTargetUserSeller || !empty($activeAuctions);
 
 require Utilities::basePath('views/account.view.php');
