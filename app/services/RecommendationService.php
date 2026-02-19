240419120206309bc39bf1370c541af1d56ad2f8
Philip Hargreaves <philiphargreaves@live.co.uk>
2026-02-19 02:18:00 +0000

    Add caching to reccomendations

--- app/services/RecommendationService.php
+++ app/services/RecommendationService.php
@@ -4,20 +4,35 @@ use app\repositories\AuctionRepository;
 
 class RecommendationService {
     private AuctionRepository $auctionRepo;
+    private array $cache = [];
+    private const CACHE_TTL = 300;
 
     public function __construct(AuctionRepository $auctionRepo) {
         $this->auctionRepo = $auctionRepo;
     }
 
     public function getRecommendedAuctions(?int $userId, int $requiredCount, int $offset = 0): array {
+        $cacheKey = ($userId ?? 'guest') . ":{$requiredCount}:{$offset}";
+
+        if (isset($this->cache[$cacheKey]) && $this->cache[$cacheKey]['expires'] > time()) {
+            return $this->cache[$cacheKey]['data'];
+        }
+
         if (is_null($userId)) {
-            $recommendedAuctions = $this->auctionRepo->getByFilters($requiredCount, 0, 'ending_soonest');
+            $results = $this->auctionRepo->getByFilters($requiredCount, 0, 'ending_soonest');
         } else {
-            $recommendedAuctions = $this->auctionRepo->getRecommendedByUserIdAndFilter(
+            $results = $this->auctionRepo->getRecommendedByUserIdAndFilter(
                 $userId, $requiredCount, $offset,
                 myLimit: 30, similarUserLimit: 100,
-                bidWeight: 3, watchlistWeight: 1 );
+                bidWeight: 3, watchlistWeight: 1
+            );
         }
-        return $recommendedAuctions;
+
+        $this->cache[$cacheKey] = [
+            'data' => $results,
+            'expires' => time() + self::CACHE_TTL,
+        ];
+
+        return $results;
     }
 }
